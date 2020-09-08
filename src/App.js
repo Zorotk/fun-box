8e6b3a63d4026a106786c6fe9d156e89c66a7e5b
Blazor <zorotkov@rambler.ru>
2020-09-08 15:28:06 +0300

    release

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
 import React, { useState, useRef } from "react";
 import Cards from "./cards/card";
 import "./App.css";
+
 const arrTaste = [
   {
     taste: "с фуа-гра",
     count: 10,
-    prize: "",
+    prize: "мышь в подарок",
     weight: "0,5",
     description: "Чего сидишь? Порадуй котэ",
-    description2: 'Печень утки разварная с артишоками.',
+    description2: "Печень утки разварная с артишоками.",
     disable: false,
+    state: false
   },
   {
     taste: "с рыбой",
     count: 40,
-    prize: 2,
+    prize:"2 мыши в подарок",
     weight: 2,
     description: "Чего сидишь? Порадуй котэ",
-    description2: 'Филе из цыплят с трюфелями в бульоне.',
+    description2: "Филе из цыплят с трюфелями в бульоне.",
     disable: false,
+    state: true
   },
   {
     taste: "с курой",
     count: 100,
-    prize: 5,
+    prize: "5 мышей в подарок",
     weight: 5,
     description: "Печалька, с курой закончился.",
     description2: "Головы щучьи с чесноком да свежайщая сёмгуша.",
     disable: true,
+    state:false
   },
 ];
 function App() {
-  const [toggle, settoggle] = useState(Array(3).fill(false));
+  const [toggle, settoggle] = useState(arrTaste);
   const toggleRef = useRef(false);
-  const toggleChange = (index) => {
+  const toggleChange = () => {
     toggleRef.current = !toggleRef.current;
-  
   };
   const mouseHandler = (index) => {
     if (toggleRef.current && !arrTaste[index].disable) {
-      toggle[index] = !toggle[index];
+      toggle[index].state = !toggle[index].state;
     }
     settoggle([...toggle]);
     toggleRef.current = false;
   };
   const onBuy = (index) => {
-    toggle[index] = !toggle[index]
-    settoggle([...toggle])
-};
+    toggle[index].state = !toggle[index].state;
+    settoggle([...toggle]);
+  };
 
   return (
     <>
